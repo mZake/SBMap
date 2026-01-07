340ba22aaef23ce96cbb62b7d9b1714f721ce64b
mZake <zakeplaybr@gmail.com>
2026-01-07 14:11:34 -0300

    Refactor error system

---
+++ source/error.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "error.h"
+
+namespace SBMap
+{
+    Error MakeError(const char* format, ...)
+    {
+        Error error;
+        
+        va_list args;
+        va_start(args, format);
+        vsnprintf(error.message, sizeof(error.message), format, args);
+        va_end(args);
+        
+        return error;
+    }
+}
--- source/error.h
+++ source/error.h
@@ -1,63 +1,68 @@
 #pragma once
 
-#include <format>
-#include <string>
 #include <variant>
+
+#include <SDL3/SDL.h>
 
 namespace SBMap
 {
     struct Error
     {
-        std::string what;
-        
-        template<typename... TArgs>
-        Error(std::format_string<TArgs...> format, TArgs&&... args)
-            : what(std::format(format, std::forward<TArgs>(args)...))
-        {}
+        char message[512];
     };
     
     template<typename TValue>
     struct Result
     {
-        std::variant<TValue, Error> result;
-        bool is_value = false;
+        std::variant<TValue, Error> value_or_error;
         
-        Result(const TValue& value)
-            : result(value)
-            , is_value(true)
-        {}
-        
-        Result(const Error& error)
-            : result(error)
-            , is_value(false)
-        {}
+        Result(const TValue& value) : value_or_error(value) {}
+        Result(const Error& error) : value_or_error(error) {}
     };
+    
+    Error MakeError(const char* format, ...);
+    
+    template<typename TValue>
+    bool IsResultValue(const Result<TValue>& result)
+    {
+        return std::holds_alternative<TValue>(result.value_or_error);
+    }
+    
+    template<typename TValue>
+    bool IsResultError(const Result<TValue>& result)
+    {
+        return std::holds_alternative<Error>(result.value_or_error);
+    }
     
     template<typename TValue>
     TValue& GetResultValue(Result<TValue>& result)
     {
-        TValue& value = std::get<TValue>(result.result);
+        SDL_assert(IsResultValue(result));
+        TValue& value = std::get<TValue>(result.value_or_error);
         return value;
     }
     
     template<typename TValue>
     const TValue& GetResultValue(const Result<TValue>& result)
     {
-        const TValue& value = std::get<TValue>(result.result);
+        SDL_assert(IsResultValue(result));
+        const TValue& value = std::get<TValue>(result.value_or_error);
         return value;
     }
     
     template<typename TValue>
     Error& GetResultError(Result<TValue>& result)
     {
-        Error& error = std::get<Error>(result.result);
+        SDL_assert(IsResultError(result));
+        Error& error = std::get<Error>(result.value_or_error);
         return error;
     }
     
     template<typename TValue>
     const Error& GetResultError(const Result<TValue>& result)
     {
-        const Error& error = std::get<Error>(result.result);
+        SDL_assert(IsResultError(result));
+        const Error& error = std::get<Error>(result.value_or_error);
         return error;
     }
 }
