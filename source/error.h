f258886447f89a101ae4563d16b354b1ac9d0202
mZake <zakeplaybr@gmail.com>
2026-01-23 11:15:49 -0300

    Refactor Result into a class

--- source/error.h
+++ source/error.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <variant>
+#include <utility>
 
 #include <SDL3/SDL.h>
 
@@ -11,58 +11,56 @@ namespace SBMap
         char message[512];
     };
     
-    template<typename TValue>
-    struct Result
+    template<typename TValue, typename TError = Error>
+    class Result
     {
-        std::variant<TValue, Error> value_or_error;
+    public:
+        Result(TValue value)
+        {
+            ::new(&m_Storage) TValue(std::move(value));
+            m_IsValue = true;
+        }
         
-        Result(const TValue& value) : value_or_error(value) {}
-        Result(const Error& error) : value_or_error(error) {}
+        Result(TError error)
+        {
+            ::new (&m_Storage) TError(std::move(error));
+            m_IsValue = false;
+        }
+        
+        ~Result()
+        {
+            if (m_IsValue)
+                m_Storage.value.~TValue();
+            else
+                m_Storage.error.~TError();
+        }
+        
+        Result(const Result&) = delete;
+        Result(Result&&) = delete;
+        
+        Result& operator=(const Result&) = delete;
+        Result& operator=(Result&&) = delete;
+        
+        bool IsValue() const { return m_IsValue; }
+        bool IsError() const { return !m_IsValue; }
+        operator bool() const { return IsValue(); }
+        
+        TValue& GetValue() { SDL_assert(IsValue()); return m_Storage.value; }
+        TError& GetError() { SDL_assert(IsError()); return m_Storage.error; }
+        const TValue& GetValue() const { SDL_assert(IsValue()); return m_Storage.value; }
+        const TError& GetError() const { SDL_assert(IsError()); return m_Storage.error; }
+        
+    private:
+        union Storage
+        {
+            TValue value;
+            TError error;
+            
+            Storage() {}
+            ~Storage() {}
+        };
+        
+        Storage m_Storage;
+        bool m_IsValue = false;
     };
-    
-    Error MakeError(const char* format, ...);
-    
-    template<typename TValue>
-    bool IsResultValue(const Result<TValue>& result)
-    {
-        return std::holds_alternative<TValue>(result.value_or_error);
-    }
-    
-    template<typename TValue>
-    bool IsResultError(const Result<TValue>& result)
-    {
-        return std::holds_alternative<Error>(result.value_or_error);
-    }
-    
-    template<typename TValue>
-    TValue& GetResultValue(Result<TValue>& result)
-    {
-        SDL_assert(IsResultValue(result));
-        TValue& value = std::get<TValue>(result.value_or_error);
-        return value;
-    }
-    
-    template<typename TValue>
-    const TValue& GetResultValue(const Result<TValue>& result)
-    {
-        SDL_assert(IsResultValue(result));
-        const TValue& value = std::get<TValue>(result.value_or_error);
-        return value;
-    }
-    
-    template<typename TValue>
-    Error& GetResultError(Result<TValue>& result)
-    {
-        SDL_assert(IsResultError(result));
-        Error& error = std::get<Error>(result.value_or_error);
-        return error;
-    }
-    
-    template<typename TValue>
-    const Error& GetResultError(const Result<TValue>& result)
-    {
-        SDL_assert(IsResultError(result));
-        const Error& error = std::get<Error>(result.value_or_error);
-        return error;
-    }
 }
